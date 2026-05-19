673fdc751d397d190539c5eec3289faa7bed14d1
CesarMtzV <mc.cesar.art@gmail.com>
2026-05-19 08:14:55 -0600

    feat: enabled clangd

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            -- LUA
             vim.lsp.enable("lua_ls")
             vim.lsp.config("lua_ls", {
                 settings = {
@@ -14,8 +15,15 @@ return {
                     }
                 }
             })
+
+            -- PYTHON
             vim.lsp.enable("pyright")
             vim.lsp.enable("prismals")
+
+            -- C
+            vim.lsp.enable("clangd")
+
+            -- KEYMAPS
             vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end)
             vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
             vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { noremap = true, silent = true })
